565be4137d76b6b1c610cc2e2e52f977613fda22
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-07-21 14:38:16 -0500

    Changed way of importing coins

--- dCC_Python_SodaMachine/wallet.py
+++ dCC_Python_SodaMachine/wallet.py
@@ -1,4 +1,4 @@
-from coins import Coin
+import coins
 
 class Wallet:
     def __init__(self):
@@ -9,10 +9,10 @@ class Wallet:
 def fill_wallet(self):
     """Method will fill wallet's money list with certain amount of each type of coin when called."""
     for index in range(8):
-        self.money.append(Coin.Quarter())
+        self.money.append(coins.Quarter())
     for index in range(10):
-        self.money.append(Coin.Dime())
+        self.money.append(coins.Dime())
     for index in range(20):
-        self.money.append(Coin.Nickel())
+        self.money.append(coins.Nickel())
     for index in range(50):
-        self.money.append(Coin.Penny())
+        self.money.append(coins.Penny())
