f51a9607d7ccfbee345bb879e6af85a684c48e1b
Fue Moua <MadisonLifeAgent@gmail.com>
2021-07-21 14:04:19 -0500

    fixed while loop line

--- dCC_Python_SodaMachine/simulation.py
+++ dCC_Python_SodaMachine/simulation.py
@@ -7,6 +7,7 @@ from customer import Customer
 class Simulation:
     def __init__(self):
         self.user_option = ()
+        self.will_proceed = True
 
     def run_simulation(self):
         """The central method called in main.py."""
@@ -14,8 +15,8 @@ class Simulation:
         soda_machine = SodaMachine()
         user_interface = User_interface()
 
-        will_proceed = False
-        while will_proceed:
+        #start the app
+        while self.will_proceed == True:
             user_option = user_interface.simulation_main_menu()
             if user_option == "1":
                 soda_machine.begin_transaction(customer)
@@ -24,4 +25,4 @@ class Simulation:
             elif user_option == "3":
                 customer.check_backpack()
             else:
-                will_proceed = False
+                self.will_proceed = False
